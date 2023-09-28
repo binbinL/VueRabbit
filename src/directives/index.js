1e0219c2b1b7004b429ddd1d75a060778eaa97a6
L-bin113 <76483058+L-bin113@users.noreply.github.com>
2023-09-28 17:59:44 +0800

    图片懒加载

---
+++ src/directives/index.js
@@ -0,0 +1,22 @@
+//定义懒加载插件
+import { useIntersectionObserver } from '@vueuse/core'
+export const lazyPlugin = {
+    install(app) {
+        //定义全局指令
+        app.directive('img-lazy', {
+            mounted(el, binding) {
+                const { stop } = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }]) => {
+                        if (isIntersecting) {
+                            //进入视口区域
+                            el.src = binding.value
+                            stop()
+                        }
+                    }
+
+                )
+            }
+        })
+    }
+}
